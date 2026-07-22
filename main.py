9403d2eda5a3695cb17c17670fed73a81d535a6c
jasper65536 <oxuguriwiw000@gmail.com>
2026-07-22 18:43:15 +0800

    添加核心项目文件（共6个文件）

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from export import save_excel
 from chart import create_chart
 
 
-mid = "519872016"
+mid = "输入你的mid"
 
-COOKIE = "buvid3=3161A2A6-117F-843B-744F-7ABFF7140E8168267infoc; b_nut=1784682868; _uuid=EEB15C1010-7426-B29E-101D9-8F57ABAF6A8468721infoc; lang=zh-Hans; buvid4=B603929E-DECD-590B-C5CB-3D7AC1D8BA0170454-026072209-RQ/ZuATPpQcdyOyYiRKm8A%3D%3D; buvid_fp=8116f6fb9b2503f208e3b8ebbaa0876f; CURRENT_QUALITY=0; rpdid=|(u~)YRJkkRu0J'u~)RY)k)~~; bili_ticket=eyJhbGciOiJIUzI1NiIsImtpZCI6InMwMyIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3ODQ5NDI4OTAsImlhdCI6MTc4NDY4MzYzMCwicGx0IjotMX0.FKzI34zSFCLsdQgVOAtKSI_JDaOQ9q247OFyezveOac; bili_ticket_expires=1784942830; home_feed_column=5; browser_resolution=1850-966; SESSDATA=a5fa7637%2C1800260765%2C0f8ce%2A71; bili_jct=6097b7950fb002a0c49b3785a5d220c6; DedeUserID=3546748521810214; DedeUserID__ckMd5=2e2c37c344f678b8; theme-tip-show=SHOWED; sid=86lx7kpc; CURRENT_FNVAL=2000; theme-avatar-tip-show=SHOWED; bp_t_offset_3546748521810214=1227828023466655744; b_lsid=E7338886_19F8944C6CC"
+COOKIE = "输入你的cookie"
 
 MAX_VIDEOS = 50
 
